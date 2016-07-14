aea727271c1bde60010b6c62ee684771d42b1834
Jeremy <saturn5971@gmail.com>
2016-07-14 00:31:37 -0400

    Added templates and have basic registration/login

---
+++ writing/storyshare/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name = 'storyshare'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+
+    url('^login/$', 
+        auth_views.login, 
+        { 'template_name': 'storyshare/login.html', },
+        name="login"),
+
+    url('^logout/$',
+        auth_views.logout,
+        { 'next_page': '/' },
+        name="logout"),
+
+    url(r'^register$', views.register, name='register'),
+]
